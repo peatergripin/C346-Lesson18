58da436a851dd0253027a792250bbcea89fe3882
Irfan bin Imran <peatergripin@gmail.com>
2026-01-16 16:59:29 +0800

    add backup data

---
+++ data.js
@@ -0,0 +1,40 @@
+const data = [
+  {
+    userid: 1,
+    fullName: "Tomas Werner",
+    email: "vhessel2w@sciencedirect.com",
+    phoneNumber: "(555) 538-2932",
+  },
+  {
+    userid: 2,
+    fullName: "Mar Docherty",
+    email: "ccornish6r@arstechnica.com",
+    phoneNumber: "(555) 622-6755",
+  },
+  {
+    userid: 3,
+    fullName: "Genia Kersey",
+    email: "wstempe40@boston.com",
+    phoneNumber: "(555) 688-9362",
+  },
+  {
+    userid: 4,
+    fullName: "Ryann Rubery",
+    email: "pludron5t@nymag.com",
+    phoneNumber: "(555) 345-6990",
+  },
+  {
+    userid: 5,
+    fullName: "Ricki Bexley",
+    email: "nbexley6g@mayoclinic.com",
+    phoneNumber: "(555) 631-9206",
+  },
+  {
+    userid: 6,
+    fullName: "Trixy Bastide",
+    email: "msleite2m@qq.com",
+    phoneNumber: "(555) 470-6816",
+  },
+];
+
+export default data;
